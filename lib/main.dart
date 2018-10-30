8fb8171e3ce68b0c67ec9619eff3f2639d80a1e3
wurikiji <wurikiji@gmail.com>
2018-10-30 16:48:17 +0900

    part 1 step #3
    https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#3

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // material 디자인이 적용된 컴포너트 패키지
+
+// part 1 수행을 위한 패키지 리스트
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random(); // 아무 영단어 생성
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center( // constant 를 제거 하기 위해 new 로 변경
+          child: new Text(wordPair.asPascalCase), // PascalCase로 표기
         ),
       ),
     );
