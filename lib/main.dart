241a06579e5ad43706fb4567e57367b006fd0740
wurikiji <wurikiji@gmail.com>
2018-10-30 17:08:47 +0900

    part 1 step #4
    https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#4
    - state 관리

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random(); // 아무 영단어 생성
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -16,9 +15,28 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: new Center( // constant 를 제거 하기 위해 new 로 변경
-          child: new Text(wordPair.asPascalCase), // PascalCase로 표기
+          child: new RandomWords(),
         ),
       ),
     );
   }
+}
+
+// Randomwords 클래스의 state를 관리할 State 클래스
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // State 클래스에 대한 초기화 이후 호출됨.
+    final wordPair = new WordPair.random(); // 아무 영단어 생성
+    return new Text(wordPair.asPascalCase); // PascalCase 로 리턴
+  }
+}
+
+// State class 인스턴스를 생성하는데 사용되는 StatefulWidget 클래스
+/* 
+  StatefulWidget 클래스를 생성하면 자동적으로 createState() 메소드를 호출하게 된다. 
+*/
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
