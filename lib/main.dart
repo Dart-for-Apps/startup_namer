63d6aaec54a24612b89b2eea428e761c9454d3a7
wurikiji <wurikiji@gmail.com>
2018-10-30 17:45:20 +0900

    part 1 step #5
    https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#5
    - Scrollable ListView 추가
      - 동적으로 List의 내용을 추가함.

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,60 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 앱 전체에 대한 title 만 지정
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center( // constant 를 제거 하기 위해 new 로 변경
-          child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
   }
 }
 
 // Randomwords 클래스의 state를 관리할 State 클래스
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[]; // 단어 쌍 저장 배열
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); // 폰트 사이즈
+
+  // State 클래스에 대한 초기화 이후 호출됨.
   @override
   Widget build(BuildContext context) {
-    // State 클래스에 대한 초기화 이후 호출됨.
-    final wordPair = new WordPair.random(); // 아무 영단어 생성
-    return new Text(wordPair.asPascalCase); // PascalCase 로 리턴
+    // 기본 material design 클래스
+    // drawer, snack bar, bottom sheet 을 제공함
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    // lazy 생성을 사용하는 ListView
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          // 단어 쌍을 한줄 출력하고, 다음 줄은 구분 bar를 그린다.
+          return new Divider();
+        }
+        final int index = i ~/ 2; // 몫의 정수 부분만 return
+
+        // 현재 보여주는 리스트의 끝에 도달한 경우
+        if (index >= _suggestions.length) {
+          // 새로운 워드 10쌍을 리스트에 추가 한다
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont, 
+      ),
+    );
   }
 }
 
